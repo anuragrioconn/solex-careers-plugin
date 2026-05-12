cddb89b420e4fb217a32f8d1c9c97a2a6719991f
anuragrioconn <developer3@rioconn.in>
2026-05-12 16:04:02 +0530

    Styling Updates and Version from 1.8.4 to 1.8.5

--- solex-careers.php
+++ solex-careers.php
@@ -3,7 +3,7 @@
  * Plugin Name: Solex Careers
  * Plugin URI: https://solex.in
  * Description: Dynamic careers listing and job detail plugin for Solex.
- * Version: 1.8.4
+ * Version: 1.8.5
  * Author: Anurag
  * Text Domain: solex-careers
  */
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) {
  * PLUGIN CONSTANTS
  */
 
-define('SOLEX_PLUGIN_VERSION', '1.8.4');
+define('SOLEX_PLUGIN_VERSION', '1.8.5');
 
 define('SOLEX_PLUGIN_URL', plugin_dir_url(__FILE__));
 
